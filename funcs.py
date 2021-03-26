3181d94a0a6132ed6714b2ace7843670d047146f
krocky-cooky <64895404+krocky-cooky@users.noreply.github.com>
2021-03-26 12:22:36 +0900

    update imshoe

--- funcs.py
+++ funcs.py
@@ -1,10 +1,11 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt 
+import os,sys
 
-def img_check(id,height = 1):
+def img_check(id,path,height = 1):
     h = 6.4*height
-    im = Image.open('./images/' + str(id) + '.jpg')
+    im = Image.open(os.path.join(path,str(id) + '.jpg'))
     im_list = np.asarray(im)
     w = h*im_list.shape[1]/float(im_list.shape[0])
     print(im_list.shape)
